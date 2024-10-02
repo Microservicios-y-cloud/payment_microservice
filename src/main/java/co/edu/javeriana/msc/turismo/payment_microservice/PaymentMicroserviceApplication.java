540ecc54678f840d912c759856e7ede86b9f1fc5
KevinVel16 <90013072+kevinevelasco@users.noreply.github.com>
2024-10-02 15:00:32 -0500

    se corrige ejecución del payment

--- src/main/java/co/edu/javeriana/msc/turismo/payment_microservice/PaymentMicroserviceApplication.java
+++ src/main/java/co/edu/javeriana/msc/turismo/payment_microservice/PaymentMicroserviceApplication.java
@@ -2,9 +2,10 @@ package co.edu.javeriana.msc.turismo.payment_microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @EnableDiscoveryClient
 public class PaymentMicroserviceApplication {
 
