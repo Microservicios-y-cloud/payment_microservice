e1581018c61b651a7984083fa676f7435aef4a16
KevinVel16 <90013072+kevinevelasco@users.noreply.github.com>
2024-10-31 22:23:36 -0500

    corrección y verificación de excepciones y envío de códigos

--- src/main/java/co/edu/javeriana/msc/turismo/payment_microservice/queue/OrderQueueConsumer.java
+++ src/main/java/co/edu/javeriana/msc/turismo/payment_microservice/queue/OrderQueueConsumer.java
@@ -2,6 +2,7 @@ package co.edu.javeriana.msc.turismo.payment_microservice.queue;
 
 import java.util.function.Consumer;
 
+import jakarta.ws.rs.ProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,11 +24,16 @@ public class OrderQueueConsumer {
 
 
         return message -> {
-            log.info("Received message: {}", message);
-            log.info("Payload: {}", message.getPayload());
-            // Se crea el evento de pago: Se hace un post del pago a la base de datos,
-            //se actualiza el saldo del usuario y el estado de la orden y del pago.
-            paymentService.processPayment(message.getPayload());
+            try{
+                log.info("Received message: {}", message);
+                log.info("Payload: {}", message.getPayload());
+                // Se crea el evento de pago: Se hace un post del pago a la base de datos,
+                //se actualiza el saldo del usuario y el estado de la orden y del pago.
+                paymentService.processPayment(message.getPayload());
+            } catch (Exception e) {
+                log.error("Error processing message: {}", e.getMessage());
+                throw new ProcessingException("Error processing payment message. Message received but could not be processed.", e);
+            }
         };
     }
 }
